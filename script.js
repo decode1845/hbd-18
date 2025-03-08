6159786056e87a134087c3caf23fd9e1d1c22b5b
decode1845 <himanshuji.274@gmail.com>
2025-03-08 23:07:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    const secretMessage = document.getElementById('secretMessage');
+    secretMessage.classList.toggle('hidden');
+});
